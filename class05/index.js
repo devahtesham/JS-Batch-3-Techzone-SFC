93134f68b4eea04487a646c6233056473c014995
Ahtesham Akram <ahte02254@gmail.com>
2023-08-08 20:43:26 +0500

    loops

---
+++ class05/index.js
@@ -0,0 +1,42 @@
+// print hello world 10 times
+// console.log("hello world");
+// ======== for loop
+/*
+    syntax:- 
+    for(initialization; condition; increement/decreement){
+
+    }
+*/
+// for(let i=1; i<=10; i++){
+//     console.log(i)
+// }
+
+// ======= while loop
+/*
+initilization
+while(condition){
+  // main logic
+ // increment/decrement
+}
+*/
+// let i = 1;
+// while(i<=10){
+//     console.log(i);
+//     i++
+// }
+
+// ======== do while
+/*
+do{
+    // body
+}while(condition)
+
+*/
+// let i = 11
+// do{
+//     console.log(i);
+//     i++
+// }while(i<=10);
+
+
+
