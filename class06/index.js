e4a1e5a28258bc60d77969ff8869a2c8d0ba19a8
Ahtesham Akram <ahte02254@gmail.com>
2023-08-11 19:14:38 +0500

    nested loops and strings method

---
+++ class06/index.js
@@ -0,0 +1,70 @@
+// ========== break, continue, nested loop
+// for(let i=0; i<=10; i++){
+//     if(i===6){
+//         // break;
+//         continue;
+//     }
+//     console.log(i);
+// }
+// for(let i=0; i<=3; i++){
+//     console.log("outer loop:- ",i);
+//     for(let j=0; j<=2; j++){
+//         console.log("inner loop:- ",j);
+//     }
+// }
+/*
+    // ========= DATA TYPES IN JS ===========
+    1. primitive
+      . String
+      . number
+      . boolean
+      . undefined
+      . null
+      . NaN
+
+    2. reference type
+      . Array
+      . objects
+      . functions   
+*/
+
+// undefined
+// let a = null;
+// console.log(a);
+// if (value === undefined);
+
+// string
+// let str = "           java      ";
+// let str = "javascript";
+
+// length
+// let ans = str.length+2
+
+// trim
+// let ans = str.trim()
+
+//split
+// let ans = str.split("")
+
+// last element
+// let ans = str[str.length-1]
+
+// getting value via index
+// let ans = str.charAt(2)
+
+
+// let ans = str.indexOf("a") 
+// let ans = str.toUpperCase().toLowerCase()
+
+// substring
+let str = "javascript";
+     //    0123456789
+// syntax
+// str.substring(start,end-1)
+let ans = str.substring(2,8)
+console.log(ans);
+
+
+
+
+
